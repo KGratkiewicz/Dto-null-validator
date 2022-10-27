08895dcebfe10c88dd6be838d959e42345923531
Jacob G <71324202+KGratkiewicz@users.noreply.github.com>
2022-10-27 11:28:31 +0200

    Update DtoValidation.java

--- DtoValidation.java
+++ DtoValidation.java
@@ -7,23 +7,23 @@ import lombok.Setter;
 import java.lang.reflect.Field;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Getter
 @Setter
 @AllArgsConstructor
 public class DtoValidation {
   private Boolean result;
-  private String messege;
+  private List<String> missingAttr;
 
-  public static DtoValidation isValid(Object dtoObject, List<String> optionalParamsNames) {
-    var fields = Arrays.stream(dtoObject.getClass().getDeclaredFields()).filter(x -> !optionalParamsNames.contains(x.getName())).toArray(Field[]::new);
-    for (var field : fields) {
-      Object fieldValue = getObjectValue(dtoObject, field);
-      if (fieldValue == null) {
-        return new DtoValidation(false, "Missing field: " + field.getName());
-      }
-    }
-    return new DtoValidation(true, "Object siutable");
+  public static DtoValidation isValid(Object dtoObject, List<String> optionalParams) {
+    var missing = Arrays.stream(dtoObject.getClass().getDeclaredFields())
+      .filter(x -> !optionalParams.contains(x.getName()))
+      .filter(x -> getObjectValue(dtoObject, x) == null)
+      .map(Field::getName)
+      .collect(Collectors.toList());
+
+    return new DtoValidation(missing.size() == 0, missing);
   }
 
   private static Object getObjectValue(Object dtoObject, Field field) {
@@ -33,4 +33,10 @@ public class DtoValidation {
       throw new RuntimeException(e);
     }
   }
+
+  public String getMessage() {
+    return "Brakujące attrybuty:" + String.join("," , this.getMissingAttr());
+  }
+}
+
 }
